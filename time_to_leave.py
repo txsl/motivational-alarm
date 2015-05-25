f06c0e891c717e98bfc149a8e46ec2f4fcc3c169
Thomas Lim <txl11@ic.ac.uk>
2015-05-25 23:16:52 +0100

    Create time_to_leave.py

---
+++ time_to_leave.py
@@ -0,0 +1,16 @@
+import datetime
+import subprocess
+
+from gtts import gTTS
+
+deadline = datetime.datetime(year=2015, month=06, day=17, minute=00, hour=16)
+
+time_remaining = deadline - datetime.datetime.now()
+
+
+
+time_remaining_str = "It's time for you to go home. Good news: only %i days of your degree left. The Triple E Department thank you for working hard on your projects." % (time_remaining.days,)
+ts = gTTS(text=time_remaining_str, lang='en')
+ts.save('time_remaining.mp3')
+
+subprocess.call(['cvlc', '--play-and-exit', '--volume', '500', 'time_remaining.mp3'])
