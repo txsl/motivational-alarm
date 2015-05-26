e4d2cf3caa51d97ac6a9856d5589ed79ac1c145d
Thomas Lim <txl11@ic.ac.uk>
2015-05-26 23:13:11 +0100

    We don't reread the same quote until we've gone through all of them

--- countdown.py
+++ countdown.py
@@ -4,17 +4,46 @@ import subprocess
 
 from gtts import gTTS
 
-quotes = [line.rstrip('\n') for line in open('quotes.txt')]
+try:
+    read_quotes_f = open('already_read.txt', 'r')
+    read_quotes = [line.rstrip('\n') for line in read_quotes_f]
+
+except IOError:
+    read_quotes_f = open('already_read.txt', 'w')
+    read_quotes = []
+
+read_quotes_f.close()
+
+print read_quotes
+
+all_quotes = [line.rstrip('\n') for line in open('quotes.txt')]
+
+all_quotes_not_read = [q for q in all_quotes if q not in read_quotes]
+
+if len(all_quotes_not_read) == 0:
+    all_quotes_not_read = all_quotes
+    open('already_read.txt', 'w').close()
+
 
 deadline = datetime.datetime(year=2015, month=06, day=17, minute=00, hour=16)
 
 time_remaining = deadline - datetime.datetime.now()
 
+motivational_quote = random.choice(all_quotes_not_read)
 
-time_remaining_str = "%i days and %i hours remaining. %s" % (time_remaining.days, time_remaining.seconds // 3600, random.choice(quotes))
+
+time_remaining_str = "%i days and %i hours remaining. %s" % (time_remaining.days, time_remaining.seconds // 3600, motivational_quote)
 
 ts = gTTS(text=time_remaining_str, lang='en')
 ts.save('time_remaining.mp3')
 
-subprocess.call(['cvlc', '--play-and-exit', '--volume', '500', 'time_remaining.mp3'])
+try:
+    subprocess.call(['cvlc', '--play-and-exit', '--volume', '500', 'time_remaining.mp3'])
+except OSError:
+    subprocess.call(['mpg123', 'time_remaining.mp3'])
+
+read_quotes_f = open('already_read.txt', 'a')
+
+read_quotes_f.write(motivational_quote + '\n')
+read_quotes_f.close()
 
