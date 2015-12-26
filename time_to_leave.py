3b5622fe7ceefbd63ea114dd4079db12c33598a2
Thomas Lim <txl11@ic.ac.uk>
2015-12-26 20:26:45 +0000

    Added more than one call, in case vlc via the terminal isn't installed (admittedly mpg123 may not be either..)

--- time_to_leave.py
+++ time_to_leave.py
@@ -13,4 +13,7 @@ time_remaining_str = "It's time for you to go home. Good news: only %i days of y
 ts = gTTS(text=time_remaining_str, lang='en')
 ts.save('time_remaining.mp3')
 
-subprocess.call(['cvlc', '--play-and-exit', '--volume', '500', 'time_remaining.mp3'])
+try:
+    subprocess.call(['cvlc', '--play-and-exit', '--volume', '500', 'time_remaining.mp3'])
+except OSError:
+    subprocess.call(['mpg123', 'time_remaining.mp3'])
